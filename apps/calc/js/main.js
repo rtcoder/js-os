61585344ddac7347f7e1931706e38210cb208e7b
Dawid Jeż <dawidjez@gmail.com>
2023-06-03 11:15:37 +0200

    fix calculator

--- apps/calc/js/main.js
+++ apps/calc/js/main.js
@@ -1,7 +1,11 @@
+const inputResult = document.querySelector("input#result");
+const numKeyboard = document.querySelector("#keyboardNum");
+const buttons = document.querySelectorAll('button');
+
 function parseCalculationString(s) {
     // --- Parse a calculation string into an array of numbers and operators
     let calculation = [],
-            current = '';
+        current = '';
     for (let i = 0, ch; ch = s.charAt(i); i++) {
         if ('^*/+-'.indexOf(ch) > -1) {
             if (current === '' && ch === '-') {
@@ -23,72 +27,88 @@ function parseCalculationString(s) {
 function calculate(calc) {
     // --- Perform a calculation expressed as an array of operators and numbers
     let ops = [{'^': (a, b) => Math.pow(a, b)},
-        {'*': (a, b) => a * b, '/': (a, b) => a / b},
-        {'+': (a, b) => a + b, '-': (a, b) => a - b}],
-            newCalc = [],
-            currentOp;
+            {'*': (a, b) => a * b, '/': (a, b) => a / b},
+            {'+': (a, b) => a + b, '-': (a, b) => a - b}],
+        newCalc = [],
+        currentOp;
+
     for (let i = 0; i < ops.length; i++) {
         for (let j = 0; j < calc.length; j++) {
             if (ops[i][calc[j]]) {
                 currentOp = ops[i][calc[j]];
             } else if (currentOp) {
                 newCalc[newCalc.length - 1] =
-                        currentOp(newCalc[newCalc.length - 1], calc[j]);
+                    currentOp(newCalc[newCalc.length - 1], calc[j]);
                 currentOp = null;
             } else {
                 newCalc.push(calc[j]);
             }
-            console.log(newCalc);
         }
         calc = newCalc;
         newCalc = [];
     }
+
+    if (!calc.length) {
+        return 0;
+    }
+
     if (calc.length > 1) {
         console.log('Error: unable to resolve calculation');
         return calc;
-    } else {
-        return calc[0];
     }
+
+    return calc[0];
 }
-$(".CalcKey").click(function () {
-    let id = $(this).attr("id"),
-            char = $(this).text();
-    if (id !== "tool5" && id !== "extra2") {
-        let inputVal = $("input#result").val();
-        $("input#result").val(inputVal + char);
-    } else {
-        switch (id) {
-            case "tool5":
-                let inputVal = calculate(parseCalculationString($("input#result").val()));
-                inputVal = inputVal.toString();
-                if (isNaN(inputVal)) {
-                    inputVal = "Error";
-                }
-                if (inputVal.indexOf('.') >= 0) {
-                    let arr = inputVal.split('.');
-                    arr[1] = arr[1].substr(0, 2);
-                    inputVal = arr.join('.');
-                }
-                $("input#result").val(inputVal);
-                break;
-            case "extra2":
-                $("input#result").val("");
-                break;
+
+function blurKeys() {
+    buttons.forEach(el => el.blur());
+}
+
+function clickKey(key) {
+    blurKeys();
+    document.querySelector(`button[data-key="${key}"]`)?.focus();
+    document.querySelector(`button[data-key="${key}"]`)?.click();
+}
+
+numKeyboard.addEventListener('click', e => {
+    const {target} = e;
+    if (!target.classList.contains('CalcKey')) {
+        return;
+    }
+    const inputValue = inputResult.value;
+    const id = target.getAttribute('id');
+    const char = target.innerText;
+    if (!["tool5", "extra2", 'extra1'].includes(id)) {
+        inputResult.value = inputValue + char;
+        return;
+    }
+    if (id === 'tool5') {
+        let inputVal = calculate(parseCalculationString(inputValue));
+        inputVal = inputVal.toString();
+        if (isNaN(inputVal)) {
+            inputVal = "Error";
         }
+        if (inputVal.indexOf('.') >= 0) {
+            let arr = inputVal.split('.');
+            arr[1] = arr[1].substr(0, 2);
+            inputVal = arr.join('.');
+        }
+        inputResult.value = inputVal;
+        return;
     }
-    $(this).blur();
-    $('#result').focus();
-});
-$("#result").keyup(function (e) {
-    e.preventDefault();
-    switch (e.keyCode) {
-        case 13://enter
-            $("#tool5").click();
-            break;
-        case 108://num dot
-            $("#numDot").click();
-            break;
-        default:
-            return false;
+    if (id === 'extra2') {
+        inputResult.value = '';
+        return;
     }
-});
+    if (id === 'extra1') {
+        inputResult.value = inputValue.replace(/.$/, '');
+    }
+})
+document.addEventListener("keydown", e => {
+    clickKey(e.key)
+    if (e.key === 'Enter' || e.key === 'NumpadEnter') {
+        clickKey('=');
+    }
+})
+document.addEventListener("keyup", blurKeys);
+document.addEventListener("mouseup", blurKeys);
